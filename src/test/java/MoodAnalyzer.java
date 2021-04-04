d39586d8c3df0af5ab120a029438af517fa5b959
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-04 20:41:05 +0530

    Added:Functionality to analyse mood happy or sad

--- src/test/java/MoodAnalyzer.java
+++ src/test/java/MoodAnalyzer.java
@@ -1,2 +1,16 @@
 public class MoodAnalyzer {
+    public String analyzeMood(String message) {
+        if(message.contains("happy"))
+        {
+            return "happy";
+        }
+        else if (message.contains("good"))
+        {
+            return "good";
+        }
+        else
+        {
+            return "sad";
+        }
+    }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
 public class MoodAnalyzerTest {
+    @Test
+    public void analyzeMood_WhenHappy_ThenReturnTrue() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("i m happy");
+        Assert.assertEquals("happy",mood);
+    }
+
+    @Test
+    public void analyzeMood_WhenSad_ThenReturnTrue() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood= moodAnalyzer.analyzeMood("i m sad");
+        Assert.assertEquals("sad",mood);
+
+    }
 }
